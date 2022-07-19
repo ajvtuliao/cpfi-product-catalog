83f0606462d12ddd3792e04fa928745eeffbb825
Alexandra <59907792+ajvtuliao@users.noreply.github.com>
2022-07-19 20:04:17 +0000

    added sort button

--- src/Body.js
+++ src/Body.js
@@ -9,7 +9,8 @@ import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+// import DropdownButton from 'react-bootstrap/DropdownButton';
 
 class Body extends React.Component {
   render() {
@@ -35,22 +36,20 @@ class Body extends React.Component {
                 />
               </Col>
               <Col lg={2} md={2}>
-                <Stack spacing={6}  sx={{ mt: 5, minWidth: '100%'}} direction="row">
+                <Stack spacing={3}  sx={{ mt: 5, minWidth: '100%'}} direction="row">
                   <Button variant="contained" size="medium" sx={{mt: 0.1, mb: 0.3, ml: -8, backgroundColor:'#05B1B0', textTransform: 'capitalize', }} startIcon={<SearchIcon fontSize="inherit" /> }><b>Search</b></Button>
-                  <DropdownButton
-                    id="dropdown-button"
-                    variant="light"
-                    menuVariant="light"
-                    title="Sort by"
-                    className="sort"
+                  <Dropdown as={ButtonGroup} 
                     size="md"
                   >
-                    <Dropdown.Item eventKey="1">Alphabetical - A to Z</Dropdown.Item>
-                    <Dropdown.Item eventKey="2">Price - Low to High</Dropdown.Item>
-                    <Dropdown.Item eventKey="3">Price - High to Low</Dropdown.Item>
-                    <Dropdown.Item eventKey="4">SKU - Low to High</Dropdown.Item>
-                    <Dropdown.Item eventKey="5">SKU - High to Low</Dropdown.Item>
-                  </DropdownButton>
+                    <Dropdown.Toggle variant="dark" className="sort-menu"><b>Sort Products</b></Dropdown.Toggle>
+                    <Dropdown.Menu className="sort" variant="dark">
+                      <Dropdown.Item eventKey="1">Alphabetical - A to Z</Dropdown.Item>
+                      <Dropdown.Item eventKey="2">Price - Low to High</Dropdown.Item>
+                      <Dropdown.Item eventKey="3">Price - High to Low</Dropdown.Item>
+                      <Dropdown.Item eventKey="4">SKU - Low to High</Dropdown.Item>
+                      <Dropdown.Item eventKey="5">SKU - High to Low</Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown>
                 </Stack>
               </Col>
             </Row>
